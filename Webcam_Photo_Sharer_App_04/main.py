7f65083c238fb2f38df5bff2c9aa8a13663d391e
Moditha Subasinghe <modithasubasinghe11@gmail.com>
2022-10-15 01:48:23 +0530

    Initial commit

--- Webcam_Photo_Sharer_App_04/main.py
+++ Webcam_Photo_Sharer_App_04/main.py
@@ -2,11 +2,31 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+import wikipedia
+import requests
+
 Builder.load_file('frontend.kv')
 
 class FirstScreen(Screen):
-    def search_image(self):
-        self.manager.current_screen.ids.img.source = 'files/design-frontend.png'
+    def get_image_link(self):
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and the first image link
+        page = wikipedia.page(query, auto_suggest=False)
+        image_link = page.images[0]
+        return image_link
+
+    def download_image(self):
+        # Download the image
+        req = requests.get(self.get_image_link())
+        image_path = 'files/picture.jpg'
+        with open(image_path,'wb') as file:
+            file.write(req.content)
+        # set the image in the Image widget
+        return image_path
+
+    def set_image(self):
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 class RootWidget(ScreenManager):
     pass
